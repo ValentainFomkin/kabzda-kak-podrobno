73f1893e174372ce4a25d59c4489ad52ef1944b3
ValentainFomkin <valentain1625@mail.ru>
2023-01-13 20:39:14 +0300

    using useDispatch()

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Rating, RatingValueType} from "./components/Rating/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {UnControledAccordion} from "./components/UnControledAccordion/UnControledAccordion";
 import {Accordion} from "./components/accordion/Accordion";
 import {UnControledOnOff} from "./components/UnControledOnOff/UnControledOnOff";
 import {OnOff} from "./components/OnOff/OnOff";
 import {UnControlledRating} from "./components/unControledRating/UnControlledRating";
+import {Select} from "./components/Select/Select";
 
 function App() {
 
     let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
     let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true)
     let [onOffValue, setOnOffValue] = useState<boolean>(true)
+    let [selectValue, setSelectValue] = useState('')
 
 
     return (
@@ -24,13 +26,24 @@ function App() {
             <UnControledAccordion titleValue={'Menu'}/>
             <Accordion titleValue={'Users'}
                        accordion={accordionCollapsed}
-                       setAccordion={setAccordionCollapsed}/>
+                       setAccordion={setAccordionCollapsed}
+                       onClick={() => {
+                       }}
+                       items={[{title: 'salam popolam', value: 2}]}/>
 
 
             <UnControledOnOff/>
             <OnOff onOffValue={onOffValue}
-                   setOnOffValue={setOnOffValue}
-            />
+                   setOnOffValue={setOnOffValue}/>
+
+
+            <Select value={selectValue}
+                    onChange={setSelectValue}
+                    items={[{title: '111', value: 1},
+                        {title: '222', value: 2},
+                        {title: '333', value: 3}
+                    ]}/>
+
 
         </div>
     );
