59d6bbfa1cdf45817a9cfaaf60789eda1dc553e3
ValentainFomkin <valentain1625@mail.ru>
2023-01-14 13:28:00 +0300

    using React.memo()

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,87 @@
+import {memo, useMemo, useState} from "react";
+
+export default {
+    title: 'useMemo'
+}
+
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        let tempResA = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 10000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResA = tempResA * i
+        }
+        return tempResA
+    }, [a])
+
+    resultB = useMemo(() => {
+        let tempB = 1
+        for (let i = 1; i <= b; i++) {
+            tempB = tempB * i
+        }
+        return tempB
+    }, [b])
+
+
+    return <>
+        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+    </>
+
+}
+
+const UserSecret = memo((props: { users: Array<string> }) => {
+    console.log('users secret')
+    return (
+        <div>
+            {props.users.map((e, i) => <div key={i}>{e}</div>)}
+        </div>
+    )
+})
+
+export const HalpsToReactMemo = () => {
+    console.log('HalpsToReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['valera', 'dimych', 'valya', 'alina'])
+
+
+    const countSum = () => setCounter(counter + 1)
+
+    const newArr = useMemo(() => {
+        return users.filter(u => u.toLowerCase().indexOf('a') > -1)
+    }, [users])
+
+    const addUser = () => {
+        const newUser = 'Sveta'
+        setUsers([
+            newUser,
+            ...users
+        ])
+    }
+
+    return <>
+        <button onClick={countSum}>+</button>
+        <button onClick={addUser}>add User</button>
+        {counter}
+        <UserSecret users={newArr}/>
+    </>
+
+
+}
